ce5c84389a638c5b378dad45cf78feca4fabac02
Ted Letcher <ted.letcher@gmail.com>
2022-09-15 15:43:58 -0400

    updated paths in MediumModelForFigure8.py

--- MediumModelForFigure8.py
+++ MediumModelForFigure8.py
@@ -4,8 +4,7 @@ Created on Fri Sep  3 16:04:42 2021
 
 @author: RDCRLJTP
 """
-import sys
-import os
+import sys,os,glob
 from matplotlib import pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,14 +15,13 @@ SnowType='Facets'
 WaveLength=np.arange(400,1600,20)  # input for GetSpectralAlbedo function
 Slab=SlabModel.SlabModel(namelist='namelist_figure8.txt')
 
+cwd = os.getcwd()
 if SnowType == 'Facets':
-    Slab.namelistDict['MasterPath']='/Users/rdcrltwl/Desktop/CRRELRTM/PaperPythonScripts/SampleData/Facets/'
+    Slab.namelistDict['MasterPath']=os.path.join(cwd,'SampleData/Facets/')
     Slab.namelistDict['PropFileNames'] = ['Facets_Optical.txt']
 else:
-    Slab.namelistDict['MasterPath']='/Users/rdcrltwl/Desktop/CRRELRTM/PaperPythonScripts/SampleData/FineGrains/'
+    Slab.namelistDict['MasterPath']=os.path.join(cwd,'SampleData/FineGrains/')
     Slab.namelistDict['PropFileNames'] = ['FineGrain_Optical.txt']
-
-print(Slab.namelistDict)
 
 Slab.Initialize()
 Azi,Zenith=1,1
